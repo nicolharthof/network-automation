908420447e580fed79d5f1607663e2aa918d4363
nicolharthof <nicol.harthof@gmail.com>
2019-11-06 19:16:56 +0100

    #3 Code refactored

--- telnetf.py
+++ telnetf.py
@@ -1,6 +1,14 @@
 import getpass
 import telnetlib
 
+
+items = ['enable', 'password', 'conf t', 'int loop 0', 'ip address 1.1.1.1 255.255.255.255', 'end', 'exit']
+
+
+def write_fn(param):
+    tn.write((param + '\n').encode('ascii'))
+
+
 HOST = input("Enter Device IP:")
 user = input("Enter your telnet username:")
 password = getpass.getpass()
@@ -8,21 +16,14 @@ password = getpass.getpass()
 tn = telnetlib.Telnet(HOST)
 
 tn.read_until(b"Username: ")
-tn.write((user + "\n").encode('ascii'))
+write_fn(user)
+
 if password:
     tn.read_until(b"Password: ")
-    tn.write((password + "\n").encode('ascii'))
-
-
-def writefn(param):
-    tn.write((param + "\n").encode('ascii'))
-
-
-items = ["enable", "cisco", "conf t", "enable", "int loop 0", "ip address 1.1.1.1 255.255.255.255",
-         "end", "exit"]
+    write_fn(password)
 
 for item in items:
-    writefn(item)
+    write_fn(item)
 
 x = tn.read_all()
-print(x)
+print(x.decode('ascii'))
